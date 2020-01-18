e149d851f7ab64939fa569024f24e1dfd139183d
Felix Zhang <18200116656@qq.com>
2020-01-18 22:59:51 +0800

    Daily commit

--- 001_Python_from_introduction_to_practice/001_plot_折线图_条形图(柱状图)_直方图_中文显示/005_ 三个条形图_多部电影的票房三天变化情况.py
+++ 001_Python_from_introduction_to_practice/001_plot_折线图_条形图(柱状图)_直方图_中文显示/005_ 三个条形图_多部电影的票房三天变化情况.py
@@ -2,17 +2,16 @@
 
 # 导入包
 import matplotlib.pyplot as plt
-import numpy as np
 
 # 显示中文和显示负号
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
 # X轴和Y轴数据,三天电影票房情况，票房单位万元
-a = ["猩球崛起3：终极之战","敦刻尔克","蜘蛛侠：英雄归来","战狼2"]
-b_14 = [2358,399,2358,362]
-b_15 = [12357,156,2045,168]
-b_16 = [15746,312,4497,319]
+movie = ["流浪地球","中国机长","哪吒之魔童降世","少年的你"]
+y_1 = [23585,4566,36898,2620]
+y_2 = [21235,5598,39852,2835]
+y_3 = [22986,6895,41250,2985]
 
 # 设置图形的大小
 plt.figure(figsize=(10, 6), dpi=128)
@@ -20,22 +19,22 @@ plt.figure(figsize=(10, 6), dpi=128)
 # 设置X轴的坐标数据,条形图宽度为0.2,先设置14号的数据，绘制15号，
 # X轴坐标向右移动0.02即可，也可以多移动一点，保持一点间隙
 bar_width = 0.2
-x_14 = list(range(len(a)))
-x_15 = [i + 0.02 + bar_width for i in x_14]
-x_16 = [i + 0.02 + bar_width for i in x_15]
+x_1 = list(range(len(movie)))
+x_2 = [i + 0.02 + bar_width for i in x_1]
+x_3 = [i + 0.02 + bar_width for i in x_2]
 
 # 竖直条形图，用的是width设置宽度，x轴参数是一个可迭代对象，一般为列表
 # 分别绘制三天的电影票房，绘制三次即可
-plt.bar(x_14, b_14, width=bar_width, label='9月14日', color='orange')
-plt.bar(x_15, b_15, width=bar_width, label='9月15日',color='grey')
-plt.bar(x_16, b_16, width=bar_width, label='9月16日',color='green')
+plt.bar(x_1, y_1, width=bar_width, label='10月1日', color='red')
+plt.bar(x_2, y_2, width=bar_width, label='10月2日',color='orange')
+plt.bar(x_3, y_3, width=bar_width, label='10月3日',color='green')
 
 # 将X轴转换为对应的字符串，只对应中间的，字就在正中间
-plt.xticks(x_15, a, rotation = 0)
+plt.xticks(x_2, movie, rotation = 0)
 
 # 设置图片，X轴，Y轴标题
-plt.title("三天单日电影票房", fontsize=24)
-plt.xlabel("票房(万元)", fontsize=14)
+plt.title("2019国庆电影票房", fontsize=24)
+plt.xlabel("单日票房(万元)", fontsize=14)
 plt.ylabel("电影名称", fontsize=14)
 
 # 设置网格,显示图例
