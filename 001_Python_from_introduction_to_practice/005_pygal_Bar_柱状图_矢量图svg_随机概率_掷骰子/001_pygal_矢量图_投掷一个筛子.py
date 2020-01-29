d338f66f2db684a79d1906e3c021f538ee371172
Felix Zhang <18200116656@qq.com>
2020-01-29 22:25:05 +0800

    Daily commit

--- 001_Python_from_introduction_to_practice/005_pygal_Bar_柱状图_矢量图svg_随机概率_掷骰子/001_pygal_矢量图_投掷一个筛子.py
+++ 001_Python_from_introduction_to_practice/005_pygal_Bar_柱状图_矢量图svg_随机概率_掷骰子/001_pygal_矢量图_投掷一个筛子.py
@@ -5,12 +5,13 @@
 import pygal
 from die import Die
 
-# 创建一个D6实例
+# 创建一个D6实例，可以用于投掷筛子
 die = Die()
 
 # 投掷几次骰子，并将结果存储在一个列表中
 results = []
 for roll_num in range(1000):
+    # roll方法投掷一次筛子，返回投掷一次的结果
     result = die.roll()
     results.append(result)
 
